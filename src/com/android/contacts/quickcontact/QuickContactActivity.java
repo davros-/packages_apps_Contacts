d88ad208d38b6d197eb2aabd3f5613ee476fb830
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-02-14 12:12:40 +0100

    Merge commit 'android-4.2.2_r1' into merge
    Conflicts:
    	res/layout/quickcontact_photo_container.xml
    	res/values-bg/strings.xml
    	res/values-cs/strings.xml
    	res/values-da/strings.xml
    	res/values-de/strings.xml
    	res/values-fi/strings.xml
    	res/values-hu/strings.xml
    	res/values-iw/strings.xml
    	res/values-ja/strings.xml
    	res/values-ko/strings.xml
    	res/values-pl/strings.xml
    	res/values-pt/strings.xml
    	res/values-ru/strings.xml
    	res/values-zh-rCN/strings.xml
    	res/values/strings.xml
    	src/com/android/contacts/dialpad/DialpadFragment.java
    
    Change-Id: Ie223344efe2d052101656d1f68c5e0c27ef93919
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>
    
    Conflicts:
    
    	res/layout/quickcontact_photo_container.xml
    	res/values-cs/strings.xml
    	res/values-da/strings.xml
    	res/values-de/strings.xml
    	res/values-hu/strings.xml
    	res/values-ja/strings.xml
    	res/values-zh-rCN/strings.xml
    	res/values/strings.xml

--- src/com/android/contacts/quickcontact/QuickContactActivity.java
+++ src/com/android/contacts/quickcontact/QuickContactActivity.java
@@ -546,7 +546,10 @@ public class QuickContactActivity extends Activity {
             if (mLookupUri == null) {
                 Log.wtf(TAG, "Lookup uri wasn't initialized. Loader was started too early");
             }
-            return new ContactLoader(getApplicationContext(), mLookupUri, false);
+            return new ContactLoader(getApplicationContext(), mLookupUri,
+                    false /*loadGroupMetaData*/, false /*loadStreamItems*/,
+                    false /*loadInvitableAccountTypes*/, false /*postViewNotification*/,
+                    true /*computeFormattedPhoneNumber*/);
         }
     };
 
@@ -587,13 +590,18 @@ public class QuickContactActivity extends Activity {
             final CheckableImageView actionView = getActionViewAt(position);
             mTrackScroller.requestChildRectangleOnScreen(actionView,
                     new Rect(0, 0, actionView.getWidth(), actionView.getHeight()), false);
+            renderSelectedRectangle(position, 0);
         }
 
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            renderSelectedRectangle(position, positionOffset);
+        }
+
+        private void renderSelectedRectangle(int position, float positionOffset) {
             final RelativeLayout.LayoutParams layoutParams =
                     (RelativeLayout.LayoutParams) mSelectedTabRectangle.getLayoutParams();
-            final int width = mSelectedTabRectangle.getWidth();
+            final int width = layoutParams.width;
             layoutParams.leftMargin = (int) ((position + positionOffset) * width);
             mSelectedTabRectangle.setLayoutParams(layoutParams);
         }
